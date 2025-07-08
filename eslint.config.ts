29b410ca80ca217be3f8af9df30c716a3cc2264a
Muhammetmyrat <bayramovmuhammetmyrat97@gmail.com>
2025-07-08 16:21:04 +0500

    fix: eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,16 +3,16 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
 	globalIgnores(['dist', 'node_modules']),
-	...tseslint.configs.recommendedTypeChecked,
 	{
 		files: ['**/*.{js,jsx,ts,tsx}'],
 		languageOptions: {
-			parser: tseslint.parser,
+			parser: tsParser,
 			parserOptions: {
 				project: ['./tsconfig.json'],
 				ecmaVersion: 'latest',
@@ -24,7 +24,7 @@ export default defineConfig([
 			}
 		},
 		plugins: {
-			'@typescript-eslint': tseslint.plugin,
+			'@typescript-eslint': tsPlugin as any,
 			react,
 			'react-hooks': reactHooks,
 			'react-refresh': reactRefresh
@@ -34,9 +34,44 @@ export default defineConfig([
 			...react.configs.recommended.rules,
 			...reactHooks.configs.recommended.rules,
 			...reactRefresh.configs.vite.rules,
+
 			'no-unused-vars': 'off',
 			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+
 			'react/react-in-jsx-scope': 'off'
+		},
+		settings: {
+			react: {
+				version: 'detect'
+			}
+		}
+	},
+	{
+		files: ['**/*.{js,jsx,ts,tsx}'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.node
+			}
+		},
+		plugins: {
+			react,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh
+		},
+		rules: {
+			...js.configs.recommended.rules,
+			...react.configs.recommended.rules,
+			...reactHooks.configs.recommended.rules,
+			...reactRefresh.configs.vite.rules,
+			'react/react-in-jsx-scope': 'off'
+		},
+		settings: {
+			react: {
+				version: 'detect'
+			}
 		}
 	}
 ])
